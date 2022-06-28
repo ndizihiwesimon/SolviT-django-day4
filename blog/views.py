ac188fe91b8aa23c0959eff716d0e624c646f482
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-28 08:18:46 +0200

    Views and forms

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from blog.forms import BlogForm
+from blog.models import Blog
 # Create your views here.
+def  blog(request):
+    blogs=Blog.objects.all()
+    context={"blogs":blogs}
+    return render(request, 'blog.html', context)
+
+def add_blog(request):
+    form=BlogForm()
+    context={"form":form}
+    if request.method == 'POST':
+        blog_form=BlogForm(request.POST)
+        if blog_form.is_valid():
+            blog_form.save()
+            return redirect("blogs")
+        else:
+            context={'form': blog_form}
+            return render(request,'add_form.html',context)
+    return redirect(request,'add_form.html',context)
