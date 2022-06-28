3238fa6ea169b5436cc341277544cc83f96e4505
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-28 08:20:37 +0200

    Forms py

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+from django.forms import forms
+from django.contrib.auth import get_user_model
+from blog.models import Blog
+User = get_user_model()
+
+
+class UserRegistration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = '__all__'
