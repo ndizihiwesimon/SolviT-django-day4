02a1bcddbc50fa08f293142e002c14975a3f75d7
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-28 08:15:13 +0200

    Configured DB

--- solvitBlog/settings.py
+++ solvitBlog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'solvitBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DayFour',
+        'USER':'postgres',
+        'HOST':'localhost',
+        'PASSWORD': 2121,
+        'PORT':5432
     }
 }
 
